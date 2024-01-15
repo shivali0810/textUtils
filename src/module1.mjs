5502b89776bb8acb94eaebfabecc3a021faa0dc7
shivali0810 <shivaligupta0810@gmail.com>
2024-01-15 20:37:50 +0530

    video 6

---
+++ src/module1.mjs
@@ -0,0 +1,8 @@
+const a="shi";
+const b="val";
+const c="gup";
+const d="ta";
+
+export default a;
+export {b};
+export {c};
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import dz, {c,b} from './module1.mjs'
+console.log(dz);
+console.log(b);
+console.log(c);
