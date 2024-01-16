06bf3b5016fa576b15f8db8d27f853a2a37d34f4
shivali0810 <shivaligupta0810@gmail.com>
2024-01-16 16:51:28 +0530

    video 8

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TextForm from './components/TextForm';
 function App() {
   return (
   <>
-<Navbar title="hehe" about="About Us"/>
+<Navbar title="TextUtils" about="About Us"/>
 <div className="container">
 <TextForm heading="write your text here" />
 </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,15 +18,26 @@ export default function TextForm(props) {
     setText(event.target.value)
   }
 
-  const[text, setText]=useState("enter text here")
+  const[text, setText]=useState("")
   return (
-    <div>
+    <>
+    <div className ="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value= {text} onChange={handleOnChange} id="myBox" rows="10" cols="15"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Case</button>
-        <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lower Case</button>
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
+        <button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
     </div>
+
+    <div className="container">
+      <h1>Your text summary</h1>
+      <p>Your text has {text.split(" ").length} words and {text.length} characters </p>
+      <p>{0.008*text.split(" ").length} minutes to read</p>
+    </div>
+
+    <h2>Preview</h2>
+    <p>{text}</p>
+    </>
   )
 }
